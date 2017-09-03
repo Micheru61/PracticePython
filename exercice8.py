7316af5dadeb3a4972fcffc71c2028eab83c69a0
Micheru61 <micheru28@hotmail.fr>
2017-09-03 17:09:28 +0200

    exercice8.py

--- exercice8.py
+++ exercice8.py
@@ -1,5 +1,15 @@
 #!usr/bin/python3.4
 
+"""
+Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner, and ask if the players want to start a new game)
+
+Remember the rules:
+
+    Rock beats scissors
+    Scissors beats paper
+    Paper beats rock
+"""
+
 hands = ['Rock', 'Paper', 'Scissors']
 
 beat_hands = {
