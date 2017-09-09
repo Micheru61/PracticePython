43feed37cf98cbbb0695a2957c34ae1657838241
Micheru61 <micheru28@hotmail.fr>
2017-09-09 15:14:58 +0200

    exercice11.py

--- exercice11.py
+++ exercice11.py
@@ -1,5 +1,12 @@
 #!usr/bin/python3.4
 
+"""
+Ask the user for a number and determine whether the number is prime or not.
+(For those who have forgotten, a prime number is a number that has no
+divisors.). You can (and should!) use your answer to Exercise 4 to help you.
+Take this opportunity to practice using functions, described below.
+"""
+
 def test_prime(number):
     divisor_numbers = 0
     divisor_numbers_list = []
